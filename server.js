7f078cefd3238319a70c7eccc0c420a1fa7f73e0
Sidney Andrews <v-sandrews@microsoft.com>
2021-06-03 10:40:34 -0400

    Added header changes

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var port = process.env.PORT || 8080;
+const fs = require('fs');
+const header = process.env.SITE_HEADER || 'Transforming the work place';
 
 var app = express();
 
-app.get('/', (req, res) =>
-    res.sendFile(__dirname + '/index.html')
-);
+app.use('/media', express.static('public'));
 
-app.use('/media', express.static('public'))
+app.use('/', (_, res) =>
+    fs.readFile(__dirname + '/index.html', (_, data) =>
+        res.send(
+            data.toString().replace('<!--header-placeholder-->', header)
+        )
+    )
+);
 
 app.listen(port, () => {
     console.log(`Application listening at http://localhost:${port}`)
-})
+});
